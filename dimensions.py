fec7ae961c0cb035092a6214ae0fa7dca6ed5b59
anntsai2356 <hikarunost@gmail.com>
2023-07-11 16:03:23 +0800

    modifies download dimensions

---
+++ dimensions.py
@@ -0,0 +1,22 @@
+class Dimensions:
+    years = ["106", "107", "108", "109"]
+    quarters = ["1", "2", "3", "4"]
+    real_estate_cities = {
+        "A": "臺北市",
+        "F": "新北市",
+        "E": "高雄市",
+    }
+    pre_sale_cities = {
+        "H": "桃園市",
+        "B": "臺中市",
+    }
+    transaction_types = {
+        "A": {
+            "name": "real estate sales",
+            "cities": real_estate_cities,
+        },
+        "B": {
+            "name": "pre-sale house sales",
+            "cities": pre_sale_cities,
+        },
+    }
--- downloader.py
+++ downloader.py
@@ -1,14 +1,3 @@
-"""
-# Question 1
-不限制方式下載【內政部不動產時價登錄網 】中,【106年第1季】~【109年第4季】、位於【臺北市/新北市/高雄市】的
-【不動產買賣】資料,【桃園市/臺中市】 的【預售屋買賣】資料,下載檔案格式選擇【CSV格式】,請選擇【非本期下載】。
-
-# Question 2
-使用【Pandas】,讀取所有檔名【 ?_lvr_land_? 】的資料集,分別建立 data frame 物件, 設定以【第二列英文】做為
-data frame 欄位標頭,並新增欄位【df_name】(內容請用程式將資料補齊,例如: 106年第1季/新北市/不動產買賣 -> 106_1_F_A、
-107年第2季/台中市/預售屋買賣-> 107_2_B_B)。
-"""
-
 import requests
 import os
 from pathlib import Path
@@ -64,49 +53,13 @@ class Downloader:
 
 
 if __name__ == "__main__":
-    years = ["106", "107", "108", "109"]
-    quarters = ["1", "2", "3", "4"]
+    from dimensions import Dimensions
 
-    real_estate_cities = {
-        "A": "臺北市",
-        "F": "新北市",
-        "E": "高雄市",
-    }
-    pre_sale_cities = {
-        "H": "桃園市",
-        "B": "臺中市",
-    }
-    transaction_types = {
-        "A": {
-            "name": "real estate sales",
-            "cities": real_estate_cities,
-        },
-        "B": {
-            "name": "pre-sale house sales",
-            "cities": pre_sale_cities,
-        },
-    }
-
-    # TEST
-    # years = ["106"]
-    # quarters = ["1"]
-
-    # real_estate_cities = {
-    #     "A": "臺北市",
-    # }
-    # pre_sale_cities = {
-    #     "B": "臺中市",
-    # }
-    # transaction_types = {
-    #     "A": {
-    #         "name": "real estate sales",
-    #         "cities": real_estate_cities,
-    #     },
-    #     "B": {
-    #         "name": "pre-sale house sales",
-    #         "cities": pre_sale_cities,
-    #     },
-    # }
+    years = Dimensions.years
+    quarters = Dimensions.quarters
+    real_estate_cities = Dimensions.real_estate_cities
+    pre_sale_cities = Dimensions.pre_sale_cities
+    transaction_types = Dimensions.transaction_types
 
     downloader = Downloader()
     downloader.run(transaction_types, years, quarters)
